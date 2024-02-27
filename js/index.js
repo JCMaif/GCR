a27ea897aff330b6e36262e6bd2c66a70f85074a
JCMaif <158159856+JCMaif@users.noreply.github.com>
2024-02-27 14:21:31 +0100

    Contact (#14)
    * page contact update
    
    * update 2 fiche contact
    
    * fiche contact JS
    
    ---------
    
    Co-authored-by: ThibaultMesmin <thibault.mesmin@maif.fr>

--- js/index.js
+++ js/index.js
@@ -196,9 +196,35 @@ displayLandingPage();
   const displayContactPage = () => {
     applicationSection.id = "contact";
     applicationSection.innerHTML = `
-      contact
-      `;
-
+    <form action="">
+        <div class="formulaire">
+            <h1>Contactez-nous</h1>
+            <div class="separation"></div>
+            <div class="corps-formulaire">
+                <div class="gauche">
+                    <div class="groupe">
+                        <i class="fa-solid fa-user"></i>
+                        <input type="text" placeholder="Votre prénom">
+                    </div>
+                    <div class="groupe">
+                        <i class="fa-solid fa-envelope"></i>
+                        <input type="text" placeholder="Votre addresse e-mail">
+                    <div class="groupe">
+                        <i class="fa-solid fa-phone-volume"></i>
+                        <input type="text" placeholder="Votre numéro de téléphone">
+                </div>
+                <div class="droite">
+                    <div class="groupe">
+                        <label for="">Message</label>
+                        <textarea placeholder="Ecrivez votre messages..."></textarea>
+                    </div>
+                </div>
+            </div>
+            <div class="pied-formulaire" align="center">
+                <button>Envoyer le message</button>
+            </div>
+        </div>
+    </form>`;
   }
 
   // -------------------Footer------------------------------
